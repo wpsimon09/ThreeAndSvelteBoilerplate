cc5dcab21277b603dabee7e2a7afb2d20055fb17
Simon P <poto0002@hz.nl>
2023-10-21 10:43:59 +0200

    added threeJS to the svelte boilerplate

--- src/lib/components/Example.svelte
+++ src/lib/components/Example.svelte
@@ -1,4 +1,8 @@
-import * as THREE from 'three';
+<script>
+    import * as THREE from 'three';
+    import {browser} from '$app/environment';
+if(browser)
+{
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
@@ -8,7 +12,7 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const geomtery = new THREE.SphereGeometry(1);
+const geomtery = new THREE.BoxGeometry(1,1,1);
 const material = new THREE.MeshBasicMaterial({color: 0x00ff00});
 const cube = new THREE.Mesh(geomtery, material);
 scene.add(cube);
@@ -23,3 +27,13 @@ function animate(){
     cube.rotation.y += 0.01;
 }
 animate();  
+}
+
+
+</script>
+
+<div class="h-screen flex item-center justify-center">
+
+ 
+
+</div>
